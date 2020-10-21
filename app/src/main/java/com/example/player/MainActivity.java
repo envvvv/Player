c22185227d89f1ca311aa7bbff5c6e903fe424d3
shmilytwj <1272158921@qq.com>
2020-10-21 17:52:43 +0800

    歌单的增删

--- app/src/main/java/com/example/player/MainActivity.java
+++ app/src/main/java/com/example/player/MainActivity.java
@@ -1,18 +1,35 @@
 package com.example.player;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,8 +38,12 @@ public class MainActivity extends AppCompatActivity {
     ProgressBar pb;
     SeekBar sbr;
     TextView tv;
+    ListView listView;
     String ID="0";
-    TextView size;
+    final int  RANDOM=1;
+    final int  INORDER=2;
+    int TYPE=INORDER;
+    List<Map<String ,String>> list=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +51,60 @@ public class MainActivity extends AppCompatActivity {
         pb = (ProgressBar)findViewById(R.id.pb);
         sbr = (SeekBar)findViewById(R.id.sbr);
         tv=(TextView)findViewById(R.id.tv);
+        listView=(ListView)findViewById(R.id.MusicList);
+
+        for(int i=0;i<=2;i++){
+        Map<String,String> map=new HashMap<>();
+        map.put("Id",String.valueOf(i));
+        map.put("Name","Song"+String.valueOf(i));
+        list.add(map);
+        }
+        setListViewAdapter(list);
+        registerForContextMenu(listView);
+        final Intent intent = new Intent(this,ServiceMusic.class);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ID=String.valueOf(i);
+                intent.putExtra("action","PlAY");
+                intent.putExtra("ID",ID);
+                startService(intent);
+                tv.setText("播放状态：正在播放Song"+ID);
+            }
+        });
+    }
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        this.getMenuInflater().inflate(R.menu.contextmenu, menu);
+    }
+    public boolean onContextItemSelected(MenuItem item) {
+        TextView Id = null;
+
+        AdapterView.AdapterContextMenuInfo info = null;
+        View itemView = null;
+
+        switch (item.getItemId()) {
+            case R.id.delete:
+                //删除单词
+                info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                itemView = info.targetView;
+                Id = (TextView) itemView.findViewById(R.id.id);
+                if (Id != null) {
+                    String strId = Id.getText().toString();
+                        onDeleteDialog(strId);
+                }
+                break;
+        }
+        return true;
+    }
+
+    private void onDeleteDialog(String strId) {
+       for(int i=0;i<list.size();i++){
+           if(list.get(i).containsValue(strId)){
+               list.remove(i);
+           }
+       }
+       setListViewAdapter(list);
     }
 
     public void PlayonClick(View view) {
@@ -53,8 +128,7 @@ public class MainActivity extends AppCompatActivity {
                     sbr.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
                         @Override
                         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-                            mybind.seekTo(progress);
+//                            mybind.seekTo(progress);
                         }
 
                         @Override
@@ -64,6 +138,7 @@ public class MainActivity extends AppCompatActivity {
 
                         @Override
                         public void onStopTrackingTouch(SeekBar seekBar) {
+                            mybind.seekTo(sbr.getProgress());
 
                         }
                     });
@@ -78,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
                             //设置当前进度
                             while (true) {
                                 pb.setProgress(mybind.getMusicCurrentPosition());
-//                                sbr.setProgress(mybind.getMusicCurrentPosition());
+                                sbr.setProgress(mybind.getMusicCurrentPosition());
                                 try {
                                     Thread.sleep(100);
                                 } catch (Exception e) {
@@ -111,67 +186,125 @@ public class MainActivity extends AppCompatActivity {
         startService(intent);
         tv.setText("播放状态：暂停播放");
     }
-    public void StoponClick(View view) {
-
-        Intent intent = new Intent(this,ServiceMusic.class);
-        intent.putExtra("action","Stop");
-        startService(intent);
-        tv.setText("播放状态：停止播放");
-    }
 
     public void PreviousonClick(View view) {
         Intent intent = new Intent(this,ServiceMusic.class);
         intent.putExtra("action","Previous");
-        if(ID=="0"){
-            ID="3";
-            intent.putExtra("ID",ID);
+        if(TYPE==RANDOM){
+            Random rand = new Random();
+            ID = String.valueOf(rand.nextInt(4));
         }
-        else{
-            ID=String.valueOf(Integer.parseInt(ID)-1);
-            intent.putExtra("ID",ID);
+        else {
+            if(Integer.parseInt(ID)<=0)
+                ID=String.valueOf(list.size()-1);
+            else
+                ID=String.valueOf(Integer.parseInt(ID)-1);
         }
-        startService(intent);
+        intent.putExtra("ID",ID);
         tv.setText("播放状态：切换上一首Song"+ID);
+        startService(intent);
+        tv.setText("播放状态：正在播放Song"+ID);
     }
 
     public void NextonClick(View view) {
         Intent intent = new Intent(this,ServiceMusic.class);
         intent.putExtra("action","Next");
-        if(ID=="3"){
-            ID="0";
-            intent.putExtra("ID",ID);
+        if(TYPE==RANDOM){
+            Random rand = new Random();
+            ID = String.valueOf(rand.nextInt(list.size()));
         }
-        else{
-            ID=String.valueOf(Integer.parseInt(ID)+1);
-            intent.putExtra("ID",ID);
+        else {
+            if(Integer.parseInt(ID)>=list.size()-1)
+                ID="0";
+            else
+                ID=String.valueOf(Integer.parseInt(ID)+1);
         }
-        startService(intent);
+        intent.putExtra("ID",ID);
         tv.setText("播放状态：切换下一首Song"+ID);
+        startService(intent);
+        tv.setText("播放状态：正在播放Song"+ID);
     }
 
-    public void RandomonClick(View view) {
-        Intent intent = new Intent(this,ServiceMusic.class);
-        intent.putExtra("action","Random");
-        Random rand = new Random();
-        ID=String.valueOf(rand.nextInt(4));
-        intent.putExtra("ID",ID);
-        startService(intent);
-        tv.setText("播放状态：随机播放");
+//    public void RandomonClick(View view) {
+//        Intent intent = new Intent(this,ServiceMusic.class);
+//        intent.putExtra("action","Random");
+//        intent.putExtra("ID",ID);
+//        startService(intent);
+//        TYPE=RANDOM;
+//        tv.setText("播放状态：随机播放");
+//        tv.setText("播放状态：正在播放Song"+ID);
+//    }
+
+//    public void InOrderonClick(View view) {
+//        Intent intent = new Intent(this,ServiceMusic.class);
+//        intent.putExtra("action","InOrder");
+//        intent.putExtra("ID",ID);
+//        startService(intent);
+//        TYPE=INORDER;
+//        tv.setText("播放状态：按序播放");
+//        tv.setText("播放状态：正在播放Song"+ID);
+//    }
+    private void setListViewAdapter(List<Map<String, String>> list) {
+
+        SimpleAdapter adapter = new SimpleAdapter(this, list,
+                R.layout.item,
+                new String[] { "Id","Name" }, new int[] { R.id.id,R.id.name });
+        listView.setAdapter(adapter);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.add:
+                addDialog();
+                break;
+            case R.id.Random:
+                TYPE=RANDOM;
+                tv.setText("播放状态：随机播放");
+                tv.setText("播放状态：正在播放Song"+ID);
+                break;
+            case R.id.InOrder:
+                TYPE=INORDER;
+                tv.setText("播放状态：按序播放");
+                tv.setText("播放状态：正在播放Song"+ID);
+                break;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 
-    public void InOrderonClick(View view) {
-        Intent intent = new Intent(this,ServiceMusic.class);
-        intent.putExtra("action","InOrder");
-        if(ID=="3"){
-            ID="0";
-            intent.putExtra("ID",ID);
-        }
-        else{
-            ID=String.valueOf(Integer.parseInt(ID)+1);
-            intent.putExtra("ID",ID);
-        }
-        intent.putExtra("ID",ID);
-        startService(intent);
-        tv.setText("播放状态：按序播放");
+    private void addDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        final View viewDialog = LayoutInflater.from(MainActivity.this).inflate(R.layout.adddialog, null, false);
+        builder.setTitle("添加歌曲").setView(viewDialog)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        EditText SongID=(EditText) viewDialog.findViewById(R.id.SongID);
+                        int d=0;
+                        for(int t=0;t<list.size();t++){
+                            if(list.get(t).containsValue(SongID.getText().toString())){
+                                d=1;
+                                Toast.makeText(MainActivity.this,"已存在Song"+SongID.getText().toString(),Toast.LENGTH_LONG).show();}
+                        }
+                        if(d==0) {
+                            Map<String, String> map = new HashMap<>();
+                            map.put("Id", SongID.getText().toString());
+                            map.put("Name", "Song" + SongID.getText().toString());
+                            list.add(map);
+                            setListViewAdapter(list);
+                        }
+                    }
+                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        builder.create().show();
     }
 }
--- app/src/main/java/com/example/player/ServiceMusic.java
+++ app/src/main/java/com/example/player/ServiceMusic.java
@@ -8,9 +8,19 @@ import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
 
 public class ServiceMusic extends Service {
     private MediaPlayer mediaPlayer;
+    final int  RANDOM=1;
+    final int  INORDER=2;
+    int TYPE=RANDOM;
+    String ID;
     public ServiceMusic() {
     }
 
@@ -57,52 +67,108 @@ public class ServiceMusic extends Service {
         return   new Mybind();
     }
 
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public void play(String ID){
+        if (mediaPlayer == null)
+        {
+            switch (ID){
+                case "0":
+                    mediaPlayer = MediaPlayer.create(this,R.raw.song0);
+                    break;
+                case "1":
+                    mediaPlayer = MediaPlayer.create(this,R.raw.song1);
+                    break;
+                case "2":
+                    mediaPlayer = MediaPlayer.create(this,R.raw.song2);
+                    break;
+                case "3":
+                    mediaPlayer = MediaPlayer.create(this,R.raw.song3);
+                    break;
+                case "4":
+                    mediaPlayer = MediaPlayer.create(this,R.raw.song4);
+                    break;
+                case "5":
+                    mediaPlayer = MediaPlayer.create(this,R.raw.song5);
+                    break;
+            }
+            mediaPlayer.start();
+    }
+
+    }
+//public String next(String ID){
+//    if(TYPE==RANDOM){
+//        Random rand = new Random();
+//        ID = String.valueOf(rand.nextInt(4));
+//    }
+//    else {
+//        if(Integer.parseInt(ID)>=3)
+//            ID="0";
+//        else
+//            ID=String.valueOf(Integer.parseInt(ID)+1);
+//    }
+//    return ID;
+//}
+
+//public String previous(String ID){
+//    if(TYPE==RANDOM){
+//        Random rand = new Random();
+//        ID = String.valueOf(rand.nextInt(4));
+//        play(ID);
+//    }
+//    else {
+//        if(Integer.parseInt(ID)<=0)
+//        ID="3";
+//        else
+//        ID=String.valueOf(Integer.parseInt(ID)-1);
+//    }
+//    return ID;
+//}
+    public int onStartCommand(final Intent intent, int flags, int startId) {
 
         //获取意图传递的信息
         String action = intent.getStringExtra("action");
-        String ID=intent.getStringExtra("ID");
+        ID=intent.getStringExtra("ID");
         switch (action)
         {
             case "Play":
-                if (mediaPlayer == null)
-                {
-                    switch (ID){
-                        case "0":
-                            mediaPlayer = MediaPlayer.create(this,R.raw.song0);
-                            break;
-                        case "1":
-                            mediaPlayer = MediaPlayer.create(this,R.raw.song1);
-                            break;
-                        case "2":
-                            mediaPlayer = MediaPlayer.create(this,R.raw.song2);
-                            break;
-                        case "3":
-                            mediaPlayer = MediaPlayer.create(this,R.raw.song3);
-                            break;
-                    }
-                    mediaPlayer.start();
-                }
-
+                    play(ID);
+                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer arg0) {
+                            mediaPlayer.reset();
+                            if(TYPE==RANDOM){
+                                Random rand = new Random();
+                                ID = String.valueOf(rand.nextInt(4));
+                                play(ID);
+                            }
+                            else {
+                                play(ID);
+                            }
+                        }
+                    });
                 break;
-            case "Stop":
             case "Previous":
             case "Next":
-            case "Random":
+            case "PlAY":
                 if (mediaPlayer !=null)
                 {
                     mediaPlayer.stop();
                     mediaPlayer.reset();
                     mediaPlayer.release();
                     mediaPlayer = null;
+                    play(ID);
                 }
                 break;
+            //                    ID=next(ID);
             case "Pause":
                 if (mediaPlayer !=null && mediaPlayer.isPlaying())
                 {
                     mediaPlayer.pause();
                 }
                 break;
+
+//            case "Random":
+//                TYPE=RANDOM;
+//            case "InOrder":
+//                TYPE=INORDER;
         }
         return super.onStartCommand(intent, flags, startId);
 }
